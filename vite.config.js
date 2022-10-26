47233f57995d1cf4cd0840613706d983f469c59b
Woongoh Cho <whdnddh@naver.com>
2022-10-26 19:10:52 +0900

    Remove dist from main branch

--- vite.config.js
+++ vite.config.js
@@ -10,8 +10,5 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  build: {
-    outDir: fileURLToPath(new URL('./www', import.meta.url))
   }
 })
